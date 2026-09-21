2d7931335f494e8330990de5a25638b2bfba71c3
Luan Reis <luannascimento.microcamp@gmail.com>
2026-09-21 16:39:48 -0300

    chore: initial project setup

---
+++ src/test/java/com/corecode/notificacao_service/NotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corecode.notificacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
